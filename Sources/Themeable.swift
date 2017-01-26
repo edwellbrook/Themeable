9b882c1485c9016359d2daa12c68890a76b50b6f
Ed Wellbrook <edwellbrook@gmail.com>
2017-01-26 01:46:49 +0000

    Support theming without forcing subclasses

--- Sources/Themeable.swift
+++ Sources/Themeable.swift
@@ -15,7 +15,7 @@ public final class ThemeManager<T: Theme> {
 
     public var theme: T {
         didSet {
-            NotificationCenter.default.post(name: ThemeNotification, object: nil, userInfo: ["theme": self.theme])
+            NotificationCenter.default.post(name: ThemeNotification, object: self, userInfo: ["theme": self.theme])
 
             // save theme for use next launch
             UserDefaults.standard.set(self.theme.identifier, forKey: CurrentThemeIdentifier)
@@ -38,17 +38,21 @@ public final class ThemeManager<T: Theme> {
 
 }
 
-public final class Themer<T: Themeable> {
+
+public final class Themer<T: Theme> {
 
     private var notificationToken: NSObjectProtocol? = nil
-    private weak var themeable: T?
+    private var functions: [FuncBox<T>] = []
+    private unowned var manager: ThemeManager<T>
 
-    public weak var manager: ThemeManager<T.ThemeType>!
 
-
-    public init(manager: ThemeManager<T.ThemeType>) {
+    public init(manager: ThemeManager<T>) {
         self.manager = manager
-        self.notificationToken = NotificationCenter.default.addObserver(forName: ThemeNotification, object: nil, queue: .main, using: self.notificationHandler)
+        self.notificationToken = NotificationCenter.default.addObserver(forName: ThemeNotification, object: manager, queue: .main, using: { [unowned self] notification in
+            if let theme = notification.userInfo?["theme"] as? T {
+                self.applyFunctions(theme: theme)
+            }
+        })
     }
 
     deinit {
@@ -57,24 +61,39 @@ public final class Themer<T: Themeable> {
         }
     }
 
-    private func notificationHandler(notification: Notification) {
-        if let theme = notification.userInfo?["theme"] as? T.ThemeType {
-            self.themeable?.apply(theme: theme)
+    private func applyFunctions(theme: T) {
+        for fn in functions {
+            fn.apply(theme)
         }
     }
 
-    public func theme(_ themeable: T) {
-        self.themeable = themeable
-        self.themeable?.apply(theme: self.manager.theme)
+    public func addThemeable<Item: Themeable>(_ item: Item) where Item.ThemeType == T {
+        // apply the current theme
+        item.apply(theme: self.manager.theme)
+
+        // box and store application function
+        let boxedFn = FuncBox<T>({ [weak item] theme in
+            item?.apply(theme: theme)
+        })
+        self.functions.append(boxedFn)
     }
 
+}
+
+private final class FuncBox<T: Theme> {
+
+    typealias ThemingFn = (_ theme: T) -> Void
+
+    let apply: ThemingFn
+
+    init(_ fn: @escaping ThemingFn) {
+        self.apply = fn
+    }
 }
 
 public protocol Themeable: class {
 
     associatedtype ThemeType: Theme
-
-    var themer: Themer<Self> { get }
 
     func apply(theme: ThemeType)
 
